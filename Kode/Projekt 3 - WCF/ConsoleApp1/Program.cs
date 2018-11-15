b36a4b2283b5a38a865f094d65e4605e6e1184d9
Andreas <androkles.al@hotmail.com>
2018-11-15 13:24:08 +0100

    DB

--- Kode/Projekt 3 - WCF/ConsoleApp1/Program.cs
+++ Kode/Projekt 3 - WCF/ConsoleApp1/Program.cs
@@ -17,16 +17,17 @@ namespace ConsoleApp1
             connection.Open();
 
             //SqlCommand cmd = new SqlCommand("select name from [Projekt3_Gruppe6].[dbo].[Country]", connection);
-            SqlCommand cmd = new SqlCommand("select * from Entertainment, Movie where Movie.entertainment_id = Entertainment.id;", connection);
+            //SqlCommand cmd = new SqlCommand("select * from Entertainment, Movie where Movie.entertainment_id = Entertainment.id;", connection);
+            SqlCommand cmd = new SqlCommand("select * from Entertainment;", connection);
 
             SqlDataReader reader = cmd.ExecuteReader();
             
 
             while(reader.Read())
             {
-                //Console.WriteLine("{0}", reader.GetValues(0));
-                Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetDateTime(4), reader.GetString(5), reader.GetString(6), reader.GetInt32(7));
-                
+                //Console.WriteLine("{0}", reader.GetString(0));
+                //Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetDateTime(4), reader.GetString(5), reader.GetString(6), reader.GetInt32(7));
+                Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}", reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetDateTime(4), reader.GetString(5), reader.GetString(6));
             }
             reader.Close();
             connection.Close();
@@ -38,6 +39,7 @@ namespace ConsoleApp1
         }
 
 
+        private string sqlFindEntertainment = "select * from Entertainment;";
         private string sqlFindMovieEntertainment = "select * from Entertainment, Movie where Movie.entertainment_id = Entertainment.id;";
         private string sqlFindSeriesEntertainment = "select * from Entertainment, Series where Series.entertainment_id = Entertainment.id;";
 

8ed392fdc31c1ba3b1f3321124d42da504bd3075
ytte5555 <ytte5555@users.noreply.github.com>
2018-11-20 12:22:13 +0100

    ..

--- Kode/Projekt 3 - WCF/ConsoleApp1/EntertainmentDB.cs
+++ Kode/Projekt 3 - WCF/ConsoleApp1/EntertainmentDB.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Projekt_3___WCF.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +13,67 @@ namespace ConsoleApp1
         private string sql_FIND_ALL_ENTERTAINMENT = "select * from Entertainment;";
         private string sql_FIND_MOVIE_ENTERTAINMENT = "select * from Entertainment, Movie where Movie.entertainment_id = Entertainment.id;";
         private string sql_FIND_SERIES_ENTERTAINMENT = "select * from Entertainment, Series where Series.entertainment_id = Entertainment.id;";
+
+
+        private SqlCommand FindAllEntertainments;
+        //con = new SqlConnection(connectionString);
+        //Navn skal findes i App.Config eller DBConnection
+        //private string _ConnectionString = ConfigurationManager.ConnectionStrings["Navn"].ConnectionString;
+
+        public EntertainmentDB()
+        {
+            //statement
+            FindAllEntertainments.CommandText = "SELECT * FROM Entertainment";
+
+        }
+
+        public List<Entertainment> GetEntertainments()
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                connection.Open();
+                using(SqlCommand cmd = connection.CreateCommand())
+                {
+
+
+
+                }
+
+            }
+
+        }
+
+        //byg entertainments
+        private List<Entertainment> buildEntertainment(SqlDataReader dr)
+        {
+            List<Entertainment> temp = new List<Entertainment>();
+            try
+            {
+
+                while (dr.)
+                {
+                    Entertainment e = FindAllEntertainments(rs.getInt("fabric_id"));
+                    Roll roll = buildRoll(rs, fab);
+                    temp.add(e);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+            return temp;
+        }
+
+
+        // Setup connection to database
+        // Instantiate SqlConnection object with connectionstringcon = new SqlConnection(connectionString);
+        // Write SQL query
+        // Instantiate SqlCommand object with query string and SqlConnection objectstring queryString = "select * from tblProduct Order by name";SqlCommand readCommand = new SqlCommand(queryString, con);
+        // Open connection con.Open();
+        // Execure SqlCommand and assign read data to a SqlDataReader objectSqlDataReader productReader = readCommand.ExecuteReader();
+        // Use data for the reader
+        // E.g. convert read "rows" to domain objectint foundRows = PopulateProductList(productReader);
+        // Close used resourcesproductReader.Close();
+
     }
 }

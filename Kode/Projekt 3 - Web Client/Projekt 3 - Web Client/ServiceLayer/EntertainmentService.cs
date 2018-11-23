e9b2aca8f5b45058e4b4da2d39080aa1a00b08cb
ytte5555 <ytte5555@users.noreply.github.com>
2018-11-23 13:08:58 +0100

    bla

---
+++ Kode/Projekt 3 - Web Client/Projekt 3 - Web Client/ServiceLayer/EntertainmentService.cs
@@ -0,0 +1,30 @@
+﻿using Projekt_3___Web_Client.Service1Reference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Service1 = Projekt_3___Web_Client.Service1Reference;
+
+
+
+namespace Projekt_3___Web_Client.ServiceLayer
+{
+    public class EntertainmentService
+    {
+        //Entertainment ent = new Entertainment();
+        public List<Entertainment> GetEntertainments()
+        {
+            Service1.
+
+            Service1.Service1Client sC = new Service1.Service1Client();
+            Array.ConvertAll(sC.FindAllEntertainments, );
+            List<Entertainment> temp = new List<Entertainment>();
+            temp = sC.FindAllEntertainments();
+
+            return sC.FindAllEntertainments();
+            
+        }
+
+
+    }
+}

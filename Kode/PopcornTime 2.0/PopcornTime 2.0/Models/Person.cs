8fe6c50dfd3cf93d198b69d2c58c0fc32fd14851
ytte5555 <ytte5555@users.noreply.github.com>
2018-12-03 14:10:54 +0100

    web client

---
+++ Kode/PopcornTime 2.0/PopcornTime 2.0/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PopcornTime_2._0.Models
+{
+        public class Person
+        {
+            
+            private string firstName;
+            private string lastName;
+            private string information;
+
+
+            public Person(string firstName, string lastName, string information)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.information = information;
+            }
+
+
+
+        }
+    }

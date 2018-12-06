bfb6f38b2c855ef55c3e67586f5ab5145ac3e043
ytte5555 <ytte5555@users.noreply.github.com>
2018-12-06 12:41:28 +0100

    popcorn

--- Kode/PopcornTime 2.0/PopcornTime 2.0/ServiceLayer/EntertainmentService.cs
+++ Kode/PopcornTime 2.0/PopcornTime 2.0/ServiceLayer/EntertainmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using EntertainmentService = PopcornTime_2._0.EntertainmentService1;
+using FavoritListService = PopcornTime_2._0.FavoritList;
 using EntertainmentModel = PopcornTime_2._0.Models;
 
 namespace PopcornTime_2._0.ServiceLayer
@@ -28,18 +29,13 @@ namespace PopcornTime_2._0.ServiceLayer
 
         private List<EntertainmentModel.Entertainment> ConvertToModelEntertainment(PopcornTime_2._0.EntertainmentService1.Entertainment[] serviceEntertainments)
         {
-            //EntertainmentService1.EntertainmentServiceClient sC = new EntertainmentService1.EntertainmentServiceClient();
-            //List<PopcornTime_2._0.EntertainmentService1.Entertainment> tempold = new List<EntertainmentService1.Entertainment>();
-            //tempold = sC.FindAllEntertainments();
 
             EntertainmentModel.Entertainment temp = null;
             List<EntertainmentModel.Entertainment> convertedEntertainments = new List<EntertainmentModel.Entertainment>();
 
-            
-
             foreach (PopcornTime_2._0.EntertainmentService1.Entertainment oldEnt in serviceEntertainments)
             {
-                
+
                 temp = new Models.Entertainment()
                 {
                     Id = oldEnt.Id,
@@ -48,26 +44,47 @@ namespace PopcornTime_2._0.ServiceLayer
                 };
 
                 //temp = new EntertainmentModel.Entertainment(oldEnt.Id, oldEnt.genre, oldEnt.title, oldEnt.country, oldEnt.language, oldEnt.releaseDate, oldEnt.storyLine, oldEnt.filmingLocation, oldEnt.information );
-                //temp.id = oldEnt.Id;
-                //temp.genre = oldEnt.genre;
-
-
-                //lav den nye entertainment type udfra den gamle
-                // 
                 convertedEntertainments.Add(temp);
             }
-
             return convertedEntertainments;
         }
 
+        public List<EntertainmentModel.FavoriteList> GetFavoriteLists()
+        {
+            FavoritListService.Service1Client fls = new FavoritListService.Service1Client();
 
-        //public Models.Entertainment entertainment
-        //{
-        //    return entertainment.title.get();
-        //}
 
-        public string title { get;  set; }
-        public DateTime releaseDate { get; set; }
+            var FavoriteLists = fls.FindAllListByUser(1);
+
+            List<EntertainmentModel.FavoriteList> convertedFavoriteList = ConvertToModelFavoriteList(FavoriteLists);
+
+            return convertedFavoriteList;
+
+        }
+
+        private List<EntertainmentModel.FavoriteList> ConvertToModelFavoriteList(PopcornTime_2._0.FavoritList.FavoriteList[] serviceFavoriteLists)
+        {
+            EntertainmentModel.FavoriteList temp = null;
+            List<EntertainmentModel.FavoriteList> convertedFavoriteList = new List<EntertainmentModel.FavoriteList>();
+
+            foreach (PopcornTime_2._0.FavoritList.FavoriteList oldFav in serviceFavoriteLists)
+            {
+
+                temp = new Models.FavoriteList()
+                {
+                    Author = oldFav.Author,
+                    Name = oldFav.Name,
+                    Description = oldFav.Description,
+                    Entertainments = oldFav.Entertainments;
+            };
+        
+            convertedFavoriteList.Add(temp);
+
+                return convertedFavoriteList;
+            
+        }
+
+
     }
 
 }
